09afd510945c1dc96ac3238bce146a18ded43de1
Ivan <chinobustos5@gmail.com>
2024-10-27 18:11:34 -0300

    proyecto terminado

--- js/config.js
+++ js/config.js
@@ -1,15 +1,16 @@
 
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-    import { getDatabase, set, ref } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
+import { getDatabase, set, ref } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
     const firebaseConfig = {
         apiKey: "AIzaSyBwS1qAt9U9DrbR1ewKJv89ycoxlAePLvo",
         authDomain: "airpods-7d2e7.firebaseapp.com",
+        databaseURL: "https://airpods-7d2e7-default-rtdb.firebaseio.com",
         projectId: "airpods-7d2e7",
         storageBucket: "airpods-7d2e7.appspot.com",
         messagingSenderId: "562713597779",
-        appId: "1:562713597779:web:f20d8fce3061c520f8f3a6"
-    };
+        appId: "1:562713597779:web:24aec5726cbff384f8f3a6"
+      };
     const app = initializeApp(firebaseConfig);
     const db = getDatabase(app);
 
@@ -20,24 +21,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebas
         set(ref(db, 'user/' + document.getElementById("username").value),
             {
                 username: document.getElementById("username").value,
+                lastname:document.getElementById("lastname").value,
                 email: document.getElementById("email").value,
-                password: document.getElementById("password").value,
-            });
-    })
+                adress:document.getElementById("adress").value,
+                phoneNumber: document.getElementById("phoneNumber").value,
 
-
-
-      //funcion que envia nuestros usuario para iniciar sesion  a firebase 
-    document.getElementById("sign-up").addEventListener('click', function (e){
-        e.preventDefault();
-        set(ref(db, 'user/' + document.getElementById("username").value),
-            {
-                email: document.getElementById("email").value,
-                password: document.getElementById("password").value,
             });
     })
 
  console.log("Conexión a Firebase establecida correctamente.");
-
-
- //inicio de sesion
